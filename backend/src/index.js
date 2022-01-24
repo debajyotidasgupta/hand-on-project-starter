645b9be0741e2e9355f53e89c58d4399b9ea68da
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2022-01-24 16:30:00 +0530

    added Login/Signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -14,8 +22,55 @@ mongoose
     console.log("Error connecting to MongoDB: ", err);
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/auth", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (!user)
+      return res.status(401).json({
+        message: "User does not exist",
+      });
+    else {
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Internal server error",
+          });
+        } else if (result) {
+          return res.status(200).json({
+            message: "User authenticated",
+          });
+        } else {
+          return res.status(400).json({
+            message: "Username/Password is incorrect",
+          });
+        }
+      });
+    }
+  });
+});
+
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      const user = new User({
+        email,
+        password: hash,
+      });
+
+      user.save();
+      return res.status(200).json({
+        message: "User created successfully",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(401).json({
+        message: "Failed Signup",
+      });
+    });
 });
 
 app.listen(process.env.PORT, () => {
